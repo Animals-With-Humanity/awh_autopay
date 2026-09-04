54682166a8b933f740489f92c6b9f15286571296
Sagar Arora <sagar.arora@innovapptive.com>
2026-09-04 15:22:05 +0530

    updated

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const db = getFirestore();
 
 /* ── Razorpay client ───────────────────────────────────────────────── */
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,9999-999-AA-99
+  key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
